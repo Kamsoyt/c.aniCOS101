ca28ecee97c7b6a45e9ea576d4dd9e7a5669c0ee
Kamsoyt <chikamso.ani@pau.edu.ng>
2025-10-23 13:46:05 +0100

    (my practice_4)

---
+++ week-4/practice_4/target/debug/deps/practice_4.d
@@ -0,0 +1,5 @@
+C:\Users\njide\OneDrive\Documents\c.aniCOS101\week-4\practice_4\target\debug\deps\practice_4.d: src\main.rs
+
+C:\Users\njide\OneDrive\Documents\c.aniCOS101\week-4\practice_4\target\debug\deps\practice_4.exe: src\main.rs
+
+src\main.rs:
