5a9dd28cecd64ed575b7d5d3ff19f46bd3e20046
Kamsoyt <chikamso.ani@pau.edu.ng>
2025-11-19 13:00:08 +0100

    (Week-8 practice_1)

---
+++ Week-7/project_1/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+
+    // using vec::new()
+    let v : vec<i64> = vec::new();
+
+    // printing the size of vector
+    println!("\nThe length of vec::new is: {}",v.len());
+
+    // Using marco
+    let v =vec!["Grace","Effiong", "Basil","Kareem","Susan"];
+
+    // printing the size of vector
+    println!("\nThe length of vec marco is: { }",v.len());
+    
+}
